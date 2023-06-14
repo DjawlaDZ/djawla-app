8c3c0566d1d2c543d411e7d8217b421e83a187bd
Oona189 <minalar09@gmail.com>
2023-06-14 21:39:49 +0000

    GuideFunctions

---
+++ src/pages/api/Guide.js
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction 
+const connectDB = require("../../libs/connectDB.js");
+import GuideSchema from '../../models/GuideSchema';
+
+
+export default async function handler(req, res) {
+    const { method } = req
+    connectDB()
+
+    switch (method) {
+      case 'GET':
+        try {
+          const list = await GuideSchema.find({})
+          res.status(200).json({ success: true, data: list })
+        } catch (error) {
+          res.status(400).json({ success: false })
+        }
+        break
+      case 'POST':
+        try {
+          const list = await GuideSchema.create(req.body)
+          res.status(201).json({ success: true, data: list })
+        } catch (error) {
+          res.status(400).json({ success: false })
+        }
+        break
+      default:
+        res.status(400).json({ success: false })
+        break
+    }
+  }
