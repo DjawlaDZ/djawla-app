c162cd2bf4c7e9dfea6c586d9a9c39f154d2118f
Amina <ka_bouzar@esi.dz>
2023-06-15 23:43:09 +0100

    merged amina's branch

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -45,7 +45,7 @@ const User = {
      wilaya: {
       required:[true,'la wilaya est obligatoire'],
       type: String,
-      enum: ['Alger','Tizi','Bejaia','Batna','Oran','Adrar','Tiaret'],
+      enum: ['Adrar', 'Chlef', 'Laghouat', 'Oum El Bouaghi', 'Batna', 'Béjaïa', 'Biskra', 'Béchar', 'Blida', 'Bouira', 'Tamanrasset', 'Tébessa', 'Tlemcen', 'Tiaret', 'Tizi Ouzou', 'Alger', 'Djelfa', 'Jijel', 'Sétif', 'Saïda', 'Skikda', 'Sidi Bel Abbès', 'Annaba', 'Guelma', 'Constantine', 'Médéa', 'Mostaganem', 'MSila', 'Mascara', 'Ouargla', 'Oran', 'El Bayadh', 'Illizi', 'Bordj Bou Arreridj', 'Boumerdès', 'El Tarf', 'Tindouf', 'Tissemsilt', 'El Oued', 'Khenchela', 'Souk Ahras', 'Tipaza', 'Mila', 'Aïn Defla', 'Naâma', 'Aïn Témouchent', 'Ghardaïa', 'Relizane'],
      }
      
   }
